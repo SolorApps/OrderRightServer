4ee37d0951fa7d691a4be847606b409b15eefa36
Erick Manrique <erickmanrique@Ericks-MacBook-Pro.local>
2016-06-22 20:12:01 -0400

    update

--- controllers/restaurantController.js
+++ controllers/restaurantController.js
@@ -45,7 +45,8 @@ module.exports = function(app) {
                     console.log(error);
                     console.log('result is empty');
                 }
-                res.send(JSON.stringify(restaurant, null, "\t"));
+                res.send(restaurant);
+                //res.send(JSON.stringify(restaurant, null, "\t"));
                 console.log(JSON.stringify(restaurant, null, "\t"));
             });
     });
