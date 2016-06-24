62161a29dc7af4ea7aa69fde008d6b86e6956def
Erick Manrique <erickmanrique@Ericks-MacBook-Pro.local>
2016-06-24 04:21:05 -0400

    update

--- controllers/restaurantController.js
+++ controllers/restaurantController.js
@@ -47,34 +47,48 @@ module.exports = function(app) {
         }
         restaurantQuery.exec(function(error, restaurant) {
             console.log('PRINTING' + error);
-            if (error == null){
-                console.log('this is null');
+
+            if (error){
+                console.log('error is detected');
+                error.status = 404;
+                return next(error);
             }
             else{
-                console.log('this is not null');
-                console.log(error);
-                console.log('not null and status');
-                console.log(error.status);
-                
+                Console.log('error is not null');
+                // res.send(restaurant);
+                res.send(JSON.stringify(restaurant, null, "\t"));
+                console.log(JSON.stringify(restaurant, null, "\t"));
             }
+
+
+            // if (error == null){
+            //     console.log('this is null');
+            // }
+            // else{
+            //     console.log('this is not null');
+            //     console.log(error);
+            //     console.log('not null and status');
+            //     console.log(error.status);
+                
+            // }
             //if (error) return next(error);
 
-                if (restaurant == undefined){
-                    err = new Error("this was err undefined");
-                    err.status = 404;
-                    //console.log(err.status);
-                    console.log('_-_undefined');
-                    next(err);
-                    //res.json(500, { error: 'message' });
-                }
-                else if (restaurant.length && restaurant.length > 0){
-                    console.log('printed no query');
-                }
-                else{
-                    console.log(restaurant.length);
-                    console.log(error);
-                    console.log('result is empty');
-                }
+                // if (restaurant == undefined){
+                //     err = new Error("this was err undefined");
+                //     err.status = 404;
+                //     //console.log(err.status);
+                //     console.log('_-_undefined');
+                //     next(err);
+                //     //res.json(500, { error: 'message' });
+                // }
+                // else if (restaurant.length && restaurant.length > 0){
+                //     console.log('printed no query');
+                // }
+                // else{
+                //     console.log(restaurant.length);
+                //     console.log(error);
+                //     console.log('result is empty');
+                // }
                 //res.send(restaurant);
                 //res.json(500, { error: 'message' });
                 //res.send(JSON.stringify(restaurant, null, "\t"));
@@ -84,11 +98,11 @@ module.exports = function(app) {
     app.use(function(err, req, res, next) {
         console.log('err status');
         console.log(err.status);
-        console.log('myerr');
-        console.log(err);
+        console.log('err message');
+        console.log(err.message);
         //console.log(req);
         //console.log(err.message);
         //res.status(err.status || 500);
-        res.json(err.status, { error: err.status });
+        res.json(err.status, { error: err.message });
     });
 }
