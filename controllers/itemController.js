6066f5f670555968327116ff0a50ff1dee47046c
Erick Manrique <erickmanrique@Ericks-MacBook-Pro.local>
2016-06-25 02:45:54 -0400

    update

--- controllers/itemController.js
+++ controllers/itemController.js
@@ -17,31 +17,54 @@ module.exports = function(app) {
             itemQuery = itemQuery.find({});
         }
         itemQuery.exec(function(error, items) {
-            res.send(JSON.stringify(items));
-            console.log(JSON.stringify(items, null, "\t"));
+            if (error){
+                error.status = 404;
+                return next(error);
+            }
+            else{
+                if (items.length && items.length > 0){
+                    res.status(200).json(items);
+                    console.log(JSON.stringify(restaurant, null, "\t"));
+                }
+                else{
+                    err = new Error("no items found");
+                    err.status = 404;
+                    next(err);
+                }
+            }
         });
     });
 
-    app.post('/api/item', function(req, res){
+    app.post('/api/createItem', function(req, res){
         if (req.body.id) {
             Item.findByIdAndUpdate(req.body.id, {
                 name: req.body.name,
                 price:req.body.price,
                 imageUrl: req.body.imageUrl
-            },function(err, todo){
-                if (err) throw err;
-                res.send('success');
+            },function(err, item){
+                if (err){
+                    error.status = 404;
+                    return next(err);
+                }
+                else{
+                    res.status(200).json({ result: 'success'});
+                }
             });
         }
         else{
-            var newTodo = Todos({
+            var newTodo = Item({
                 name: req.body.name,
                 price:req.body.price,
                 imageUrl: req.body.imageUrl
             });
             newTodo.save(function(err){
-                if (err) throw err;
-                res.send('success');
+                if (err){
+                    error.status = 404;
+                    return next(err);
+                }
+                else{
+                    res.status(200).json({ result: 'success'});
+                }
             });
         }
     });
--- controllers/restaurantController.js
+++ controllers/restaurantController.js
@@ -46,16 +46,4 @@ module.exports = function(app) {
             }
         });
     });
-
-
-    // app.get('*', function(req, res, next) {
-    //     var err = new Error('oops! something broke');
-    //     err.status = 404;
-    //     next(err);
-    // });
-    // app.use(function(err, req, res, next) {
-    //     console.log(err.status);
-    //     console.log(err.message);
-    //     res.status(err.status || 500).json({ error: err.message});
-    // });
 }
