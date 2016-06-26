317de0c729f15bc6eebf8c08d25448218ef92617
Erick Manrique <erickmanrique@Ericks-MacBook-Pro.local>
2016-06-25 22:18:37 -0400

    update

--- controllers/menuController.js
+++ controllers/menuController.js
@@ -34,7 +34,7 @@ module.exports = function(app) {
             var section = req.body.section;
             Menu.findByIdAndUpdate(req.body.id, {
                 '$addToSet': {
-                    "Appetizers":{
+                    Appetizers:{
                             items: req.body.itemId
                     }
                 }
